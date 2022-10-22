9453690e20ce88d2db1a1c2e9d3278d7f004e18f
yuki saito <ykst0622@gmail.com>
2022-10-22 23:52:18 +0900

    :sparkles: ユーザーアカウント登録画面を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  enum gender: { man: 0, woman: 1}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
