9840ede0be2d120957c557e78efa97539efa052b
yuki saito <ykst0622@gmail.com>
2022-10-23 14:42:12 +0900

    :bookmark: enum genderを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  enum gender: { man: 0, woman: 1}
+  enum gender: { 男性: 0, 女性: 1}
 
   def age
     today = Date.today.strftime("%Y%m%d").to_i
