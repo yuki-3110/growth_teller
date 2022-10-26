6d51b9aa9f5871724e857611e4fc0b0adb1b68cc
yuki saito <ykst0622@gmail.com>
2022-10-27 00:56:32 +0900

    :bookmark: ユーザーモデルのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+  validates :birthday, presence: true
+  validates :gender, presence: true
   validates :password, presence: true, on: :create
 
   enum gender: { man: 0, woman: 1}
