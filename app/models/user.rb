cbcafd59f84ec8f2f082c0dc123c7a35960a9212
yuki saito <ykst0622@gmail.com>
2022-11-01 23:04:13 +0900

    :bookmark: マイページの性別を日本語表示に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :gender, presence: true
   validates :password, presence: true, on: :create
 
-  enum gender: { man: 0, woman: 1}
+  enum gender: { 男性: 0, 女性: 1}
 
   has_many :careers, dependent: :destroy
   has_many :experiences, dependent: :destroy
